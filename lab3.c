a667ae109e1055ce1a2d734f919fbd1b8f4aec3e
valeriia08 <130174093+valeriia08@users.noreply.github.com>
2023-05-17 15:44:34 +0300

    lab3.c

--- lab3.c
+++ lab3.c
@@ -1,27 +1,29 @@
-#include<stdio.h>
-#include<math.h>
-int main() {
-double a,b,y;
-int c;
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+  double a, b, y;
+  int c;
 
-printf("Введіть дійсні значення змінних a та b:");
-scanf("%lf %lf", &a,&b);
+  printf("Введіть дійсні значення змінних a та b:");
+  scanf("%lf %lf", &a, &b);
 
-printf("Введіть число c:");
-scanf("%d", &c);
+  printf("Введіть число c:");
+  scanf("%d", &c);
 
-if (c>0){
-    if 
+  if (c > 0)
+  {
     y = sqrt(b - a) + 2 * sqrt(c);
-    
-}else if (c==0){
-    if
+  }
+  else if (c == 0)
+  {
     y = b / (c - 6 * a);
-}else {
+  }
+  else
+  {
     y = b + 12 * pow(c, 3);
-}
-printf("y=%lf", y);
+  }
+  printf("y=%.2lf", y);
 
-return 0;
-
+  return 0;
 }
